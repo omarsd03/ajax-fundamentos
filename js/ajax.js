87713769b2ec75780187f65dbfacb4f5aa6f292a
OmarSD <salgadodiaz_wwe@outlook.com>
2019-10-02 23:42:06 -0500

    Primer commit

---
+++ js/ajax.js
@@ -0,0 +1,32 @@
+(function(){
+
+    $.ajax({
+
+        type: 'GET',
+        url: 'http://www.json-generator.com/api/json/get/ceUDERTOYy?indent=2',
+        dataType: 'json'
+
+    }).done(function (data) {
+
+        console.log('Se realizo correctamente');
+
+        var person = data;
+        console.log(data);
+
+        $("#picFoto").attr("src", data.picture);
+        $("#txtNombre").val(data.name);
+        $("#txtDireccion").val(data.address);
+        $("#txtTelefono").val(data.phone);
+        $("#txtGenero").val(data.gender);
+
+    }).fail(function () {
+
+        console.log('Fallo');
+
+    }).always(function() {
+
+        console.log('Completo!');
+
+    });
+
+})();
